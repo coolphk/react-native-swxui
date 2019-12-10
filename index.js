99b9cb388777c78e9463f09e3703533a02bdf572
Administrator <25145168@qq.com>
2019-12-10 14:34:34 +0800

    aaaa

---
+++ index.js
@@ -0,0 +1,14 @@
+import {Text} from 'react-native';
+
+
+import React, {Component} from 'react';
+
+class Index extends Component {
+  render() {
+    return (
+      <Text>aaaaaa</Text>
+    )
+  }
+}
+
+export default Index;
